8e6cce7da578b90dd97f5d3bd436aca8b9870176
Celso Patiri <celsobenedetti2@gmail.com>
2022-09-23 12:03:47 -0300

    new: parkingLot DTO, service, repository and controller

---
+++ src/main/java/com/api/parkingcontrol/dtos/ParkingSpotDto.java
@@ -0,0 +1,40 @@
+package com.api.parkingcontrol.dtos;
+
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParkingSpotDto {
+
+    @NotBlank
+    private String parkingSpotNumber;
+
+    @NotBlank
+    @Size(min = 7, max = 7)
+    private String licensePlateCar;
+
+    @NotBlank
+    private String carBrand;
+
+    @NotBlank
+    private String carModel;
+
+    @NotBlank
+    private String carColor;
+
+    @NotBlank
+    private LocalDateTime registrationDate;
+
+    @NotBlank
+    private String responsibleName;
+
+    @NotBlank
+    private String block;
+
+}
---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,24 @@
+package com.api.parkingcontrol.services;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+@Service
+public class ParkingSpotService {
+
+    final ParkingSpotRepository parkingSpotRepository;
+
+    ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
+        this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
+    }
+
+}
